436f22093bc1ad1bce8197c1dd3196e5cb0176b9
ingted <ingted@gmail.com>
2015-11-13 01:37:48 -0800

    Add dexx

---
+++ test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+a=1
+echo $a
+echo $$
+bash << 'SSH_L1'
+		echo "$a"
+		echo $$
+		pwd
+		echo $1
+#                if [ "$1" == "-a" ]; then
+                        for id in $(dls -ll); do
+                                echo "$id: "
+                                docker exec $id ${*:2}
+                        done
+#                else
+#                        docker exec $1 ${@:2}
+#                fi
+
+SSH_L1
+
