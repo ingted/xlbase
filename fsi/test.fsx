e84db3a7f643a30b5d0b75cc453d000de62998ea
ingted <ingted@gmail.com>
2015-11-15 07:25:14 -0800

    Add fs scripts

---
+++ fsi/test.fsx
@@ -0,0 +1,20 @@
+﻿#r "System.ServiceProcess"
+#load "EnvironmentHelper.fsx"
+
+Console.WriteLine(EnvironmentHelper.isMono)
+
+
+module env = EnvironmentHelper
+module ProcessHelper =
+
+  open System
+  open System.ComponentModel
+  open System.Diagnostics
+  open System.IO
+  open System.Threading
+  open System.Collections.Generic
+  open System.ServiceProcess
+  open System.Configuration
+
+  let pwd () = Directory.GetCurrentDirectory()
+  let ttc = env.isMono
