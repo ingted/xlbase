ab8ac764bfa231d572817448c749ea9cd41cf0b9
ingted <ingted@gmail.com>
2015-11-24 18:38:33 -0800

    Mod 130_ha.sh in case of no pcmk mode

--- dockerfiles/addfiles/130_ha.sh
+++ dockerfiles/addfiles/130_ha.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-service pcsd restart || true
-service corosync restart || true
-service pacemaker restart || true
+o=$(svcs)
 
+g1=$(echo $o|grep \ pcsd\ )
+if [ "$g1" != "" ]; then service pcsd restart; fi
+
+g2=$(echo $o|grep \ corosync\ )
+if [ "$g2" != "" ]; then service corosync restart; fi
+
+g3=$(echo $o|grep \ pacemaker\ )
+if [ "$g1" != "" ]; then service pacemaker restart; fi
