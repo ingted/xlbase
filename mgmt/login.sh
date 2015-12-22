ff2f5bd1a7dcf93e404fdcf9edf8e4a505c44c66
anibal.yeh <anibal.yeh@gmail.com>
2015-12-22 15:56:14 -0800

    installation commit

--- mgmt/login.sh
+++ mgmt/login.sh
@@ -37,9 +37,12 @@ for host in $hosts; do
 		if [ "$IFPS1" == "" ]; then 
 			echo 'export PS1="\[\e[0;33m\][\$(date  +\"%T\")]\[\e]0;\u@\$(hostname):\ \w\a\]${debian_chroot:+(\$debian_chroot)}\[\033[01;36m\]\u@\$(hostname)\[\033[00m\]:\[\033[01;32m\]\w\[\033[00m\]\\\$ "' >> ~/.bashrc
 			echo "#ps1ed" >> ~/.bashrc
-			echo "nameserver 8.8.8.8" >> /etc/resolv.conf
-			echo "nameserver 168.95.192.1" >> /etc/resolv.conf
+
 		fi
+		sed '/8\.8\.8\.8/d' /etc/resolv.conf
+		sed '/168\.95\.1\.1/d' /etc/resolv.conf
+		echo "nameserver 8.8.8.8" >> /etc/resolv.conf
+		echo "nameserver 168.95.1.1" >> /etc/resolv.conf
 EOF
 	ssh $cip << EOF
 		bash -c 'echo \"$host\" > /etc/hostname'
