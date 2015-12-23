70b55a03d45fa4a15d35dc5778a54b20a07d76c9
anibal.yeh <anibal.yeh@gmail.com>
2015-12-23 07:52:28 -0800

    installation commit

--- mgmt/login.sh
+++ mgmt/login.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 
+
+
 echo -n Enter the cluster name:
 read cluster
 
@@ -19,12 +21,19 @@ fi
 
 echo -e "\npreparing login..."
 
-hosts=$(./mgmt-xl-get-host-by-role docker $cluster);
+expp=$(which expect)
+if [ "$expp" == "" ]; then
+	apt-get -y install expect
+fi
 
+hosts=$(./mgmt-xl-get-host-by-role docker $cluster);
+dnss=$(./mgmt-xl-get-dns $cluster)
 #Because the containers are not ready yet, so not do this 
 # (key scan should not be performed)
 # (add container hosts to /etc/hosts still needed) now...
 allhosts=$(./mgmt-xl-get-host-by-role -a $cluster); 
+
+
 for host in $hosts; do
 	echo ./mgmt-xl-get-ip $host $cluster
 	cip=$(./mgmt-xl-get-ip $host $cluster)
@@ -33,16 +42,19 @@ for host in $hosts; do
 	ssh-keyscan -H $cip >> ~/.ssh/known_hosts
 	./login.expect $cip $password > /dev/null
 	ssh $cip << 'EOF'
+		touch ~/.hushlogin
 		IFPS1=$(grep ps1ed ~/.bashrc)
 		if [ "$IFPS1" == "" ]; then 
 			echo 'export PS1="\[\e[0;33m\][\$(date  +\"%T\")]\[\e]0;\u@\$(hostname):\ \w\a\]${debian_chroot:+(\$debian_chroot)}\[\033[01;36m\]\u@\$(hostname)\[\033[00m\]:\[\033[01;32m\]\w\[\033[00m\]\\\$ "' >> ~/.bashrc
 			echo "#ps1ed" >> ~/.bashrc
 
 		fi
-		sed '/8\.8\.8\.8/d' /etc/resolv.conf
-		sed '/168\.95\.1\.1/d' /etc/resolv.conf
+		sed -i '/8\.8\.8\.8/d' /etc/resolv.conf
+		sed -i '/168\.95\.1\.1/d' /etc/resolv.conf
 		echo "nameserver 8.8.8.8" >> /etc/resolv.conf
 		echo "nameserver 168.95.1.1" >> /etc/resolv.conf
+		ssh-keygen -R $host
+		ssh-keyscan -H $host >> ~/.ssh/known_hosts
 EOF
 	ssh $cip << EOF
 		bash -c 'echo \"$host\" > /etc/hostname'
@@ -52,6 +64,7 @@ EOF
 	for ah in $allhosts; do
 		ahip=$(./mgmt-xl-get-ip $ah $cluster)
 		ahip_r=${ahip//./\\\.}
+
 		ssh $cip << EOF
 			cp /etc/hosts /etc/hosts.tmp
 			sed -i "/$ahip_r/d" /etc/hosts.tmp
@@ -62,10 +75,23 @@ EOF
 			#ssh-keygen -R $ahip
 			#ssh-keyscan -H $ahip >> ~/.ssh/known_hosts
 EOF
+	done
+
+	for ad in $dnss; do
+		ad_r=${ad//,/ }
+		ad_ipo=($ad_r)
+		ad_ip=${ad_ipo[0]}
+		echo =======================================================
+		echo $ad_r $ad_ipo $ad_ipi
+		echo =======================================================
+		ssh $cip << EOF
+			cp /etc/hosts /etc/hosts.tmp
+			sed -i "/$ad_ip/d" /etc/hosts.tmp
+			echo "$ad_r" >> /etc/hosts.tmp
+			cp /etc/hosts.tmp /etc/hosts -f
+EOF
 
 	done
-	ssh-keygen -R $host
-	ssh-keyscan -H $host >> ~/.ssh/known_hosts
 
 
 done
