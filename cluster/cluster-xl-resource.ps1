96c64858d2a070fcbd0e55039e6efc2ff4954fa6
anibal.yeh <anibal.yeh@gmail.com>
2016-02-03 14:11:42 +0800

    mgmt-xl-init resource file mod

--- cluster/cluster-xl-resource.ps1
+++ cluster/cluster-xl-resource.ps1
@@ -6,10 +6,10 @@ param(
 gc "/addfiles/resource_pcmk/$role"|%{
 	$str = $_
 	
-	if($_ -match "###cfgrole###"){"role=`"$role`""}
-	elseif($_ -match "###cfgip###"){"ip=`"$ip`""}
-	elseif($_ -match "###cfgport###"){"port=`"$port`""}
-	elseif($_ -match "###cfgpgd###"){"pgdir=`"$pgdir`""}
+	if($str -match "###cfgrole###"){"role=`"$role`""}
+	elseif($str -match "###cfgip###"){"ip=`"$ip`""}
+	elseif($str -match "###cfgport###"){"port=`"$port`""}
+	elseif($str -match "###cfgpgd###"){"pgdir=`"$pgdir`""}
 	else{$str}
 	
 }|out-file "/etc/init.d/$role" -encoding ascii -force
