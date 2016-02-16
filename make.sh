5442a9076e5e3dc3b98e75a26c016d7efab1d63d
anibal.yeh <anibal.yeh@gmail.com>
2016-02-16 14:43:24 +0800

    zfs supported

--- make.sh
+++ make.sh
@@ -2,6 +2,13 @@
 t1=$(date)
 (make)||(echo "Started @ $t1"; t2=$(date); echo "Ended   @ $t2")
 source ~/.bashrc
+
+apt-get -y install software-properties-common
+add-apt-repository -y ppa:zfs-native/stable
+apt-get -y update
+apt-get -y install -y ubuntu-zfs
+modprobe zfs
+
 export DISABLEBASE=1
 export DISABLEPACE=1
 export DISABLEUTIL=1
