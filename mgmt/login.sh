a93e2578de60868366656bbfab10aec7c2ff9031
anibal.yeh <anibal.yeh@gmail.com>
2016-03-21 15:36:40 +0800

    mgmt/login.sh interact theone

--- mgmt/login.sh
+++ mgmt/login.sh
@@ -178,13 +178,13 @@ if [ $notAnsible == 1 ]; then
         for dhost in $dhosts; do
 		#sudo sed -i "/$theone/d" /etc/sudoers
 		#sudo bash -c "echo \"$theone ALL=NOPASSWD:ALL\" >> /etc/sudoers"
-		cat ../alpha/h1/id_rsa | ssh $theone@$dhost 'cat >> .ssh/id_rsa.tmp'
-		cat ../alpha/h1/id_rsa.pub | ssh $theone@$dhost 'cat >> .ssh/id_rsa.pub.tmp' 
+		cat ../alpha/h1/id_rsa | ssh $theone@$dhost 'cat >> ~/.ssh/id_rsa.tmp'
+		cat ../alpha/h1/id_rsa.pub | ssh $theone@$dhost 'cat >> ~/.ssh/id_rsa.pub.tmp' 
 		ssh $theone@$dhost << EOF
-			chmod +w .ssh/id_rsa .ssh/id_rsa.pub;
-			cp .ssh/id_rsa.tmp .ssh/id_rsa -f
-			cp .ssh/id_rsa.pub.tmp .ssh/id_rsa.pub -f
-			chmod 500 .ssh/id_rsa .ssh/id_rsa.pub
+			chmod +w ~/.ssh/id_rsa ~/.ssh/id_rsa.pub;
+			cp ~/.ssh/id_rsa.tmp ~/.ssh/id_rsa -f
+			cp ~/.ssh/id_rsa.pub.tmp ~/.ssh/id_rsa.pub -f
+			chmod 500 ~/.ssh/id_rsa ~/.ssh/id_rsa.pub
 EOF
 		#ssh $theone@$dhost ls -l ~/.ssh/
 		expect << EOF
